298b0a06175e6cee9b91c3eec31f4b3e8a4b79b1
Riley Ho <riley@rileychh.dev>
2024-11-20 16:51:19 +0800

    feat: extend Fragment with runOnUiThread

--- app/src/main/java/com/example/kotlinhomework3/BodyMetricsFragment.kt
+++ app/src/main/java/com/example/kotlinhomework3/BodyMetricsFragment.kt
@@ -84,4 +84,9 @@ class BodyMetricsFragment : Fragment() {
         }.start()
     }
 }
+
+fun Fragment?.runOnUiThread(action: () -> Unit) {
+    this ?: return
+    if (!isAdded) return // Fragment not attached to an Activity
+    activity?.runOnUiThread(action)
 }
